5939b885a04685490021832d984c2c632efc8285
Tushar Patil <149461408+tp1243@users.noreply.github.com>
2025-06-13 09:23:41 +0530

    Update Navbar.js
    no need

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import './Navbar.css';
 
-function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <span className="logo">ChatGPT UI</span>
-      </div>
-      <div className="navbar-right">
-        <button className="nav-btn login">Login</button>
-        <button className="nav-btn signup">Sign Up</button>
-      </div>
-    </nav>
-  );
-}
-
-export default Navbar;
