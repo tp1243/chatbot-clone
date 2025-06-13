15f8fc955ac2226ace512e789d13d5ad951b88ce
Tushar Patil <149461408+tp1243@users.noreply.github.com>
2025-06-13 09:05:46 +0530

    Update firebase.js
    use your firebase configuration

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,13 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-URtIHoLXPjht38NoFIuDdRgTRkISarA",
-  authDomain: "chatgpt-21b6b.firebaseapp.com",
-  databaseURL: "https://chatgpt-21b6b-default-rtdb.firebaseio.com",
-  projectId: "chatgpt-21b6b",
-  storageBucket: "chatgpt-21b6b.firebasestorage.app",
-  messagingSenderId: "895065749709",
-  appId: "1:895065749709:web:e262f3f53df557522ad184"
+ // your firebase configuration
 };
 
 const app = initializeApp(firebaseConfig);
